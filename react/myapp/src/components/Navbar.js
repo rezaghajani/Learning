77e69edb9eca334a8c74c96c2511c56c64786bdb
Reza Aghajani <rezaaghajani@protonmail.com>
2020-04-21 18:06:12 +0430

    Learn High Order Components

--- react/myapp/src/components/Navbar.js
+++ react/myapp/src/components/Navbar.js
@@ -1,10 +1,7 @@
 import React from "react";
-import {Link,NavLink,withRouter} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 
 const Navbar = (props) => {
-  setTimeout(() => {
-    props.history.push('/Home')
-  },50)
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
