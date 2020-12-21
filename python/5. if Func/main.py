00614233cc9d0698e22ff9ba4c8d55e877a8783d
Reza Aghajani <rezaaghajani@protonmail.com>
2020-12-21 13:25:35 +0330

    Learn about if Functions in Python

---
+++ python/5. if Func/main.py
@@ -0,0 +1,18 @@
+if True:
+    print("Success")
+
+if False:
+    print("Success") # This function will not work
+
+if 3 > 2: # This is True so function will run
+    print("Success")
+
+if 4 > 5:
+    print("Success")
+else:
+    print("Failed") # Output = Failed
+
+if 4 > 5:
+    print("Success")
+elif 4 < 5 and 3 > 2:
+    print("Failed") # Output = Failed
