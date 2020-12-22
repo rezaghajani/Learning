eae2e7fdaaa2fa2b01a3bc32e73493e5594f5472
Reza Aghajani <rezaaghajani@protonmail.com>
2020-12-22 15:01:18 +0330

    Learn about Built-in functions in Python ~✔

---
+++ python/6. Built-in Functions/main.py
@@ -0,0 +1,10 @@
+max('Hello World!') # Output = 'W'
+
+min("Hello World!") # Output = ' '
+
+len('Hello World!') # Output = '11'
+
+int('32') # Convert str to int
+
+float(32) # conver int to float
+
