a1971841057001fa4e509aca4d6649bb05815b02
Reza Aghajani <rezaaghajani@protonmail.com>
2020-12-23 15:13:29 +0330

    Learn Lists in Python ✔

---
+++ python/13. Lists/main.py
@@ -0,0 +1,31 @@
+friends = ["Ali" , "Reza" , "Mahdi" , "Ahmad"]
+
+print(friends[0])
+
+for friend in friends:
+    print(friend)
+
+
+a = [1,2,3]
+b = [4,5,6]
+c = a + b
+print(c)
+
+d = c * 2
+print(d)
+
+print(d[2:5])
+
+d.append("In Taze ezafe shod")
+print(d)
+d.sort()
+print(d)
+
+x = ["a","b"]
+y = ["c" , "d"]
+x.extend(y)
+print(x)
+x.pop(1)
+print(x)
+x.remove("d")
+print(x)
