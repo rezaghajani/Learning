df5cef893ad2fd17b0b5e5ca2282693ff97362d8
Reza Aghajani <rezaaghajani@protonmail.com>
2021-01-09 11:20:26 +0330

    Solve Quera's Hard diet Problem ✔

---
+++ python/Training/diet.py
@@ -0,0 +1,26 @@
+chars = input("")
+values = []
+red = 0
+yellow = 0
+green = 0
+
+for char in chars:
+    values.append(char)
+    
+    
+for i in range(len(values)):
+    if values[i] == "G":
+        green = green +1
+    elif values[i] == "R":
+        red = red + 1
+    elif values[i] == "Y":
+        yellow = yellow + 1
+
+if red >= 3:
+    print("nakhor lite")
+elif red >= 2 and yellow >= 2:
+    print("nakhor lite")
+elif red == 5 or yellow == 5:
+    print("nakhor lite")
+else:
+    print("rahat baash")
