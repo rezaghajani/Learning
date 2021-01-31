b6550810083df4e5522b07fa222fea79dcfecd30
Reza Aghajani <rezaaghajani@protonmail.com>
2021-01-31 10:39:41 +0330

    Learn about Async and Await Parameters

--- Async-Js/app.js
+++ Async-Js/app.js
@@ -1,13 +1,24 @@
-
-console.log('Start')
-
-function userLogin(email,password,callback) {
-    setTimeout(() => {
-        console.log("Data Gathered!")
-        callback({userEmail: email})
-    },5000)
+let loginUser = (email,pass) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve({userEmail: email})
+        },2000 )
+    })
 }
 
-const user = userLogin('def@gmail.com',1234444,user => {console.log(user)})
+let userVideos = (email) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(['video1','video2','video3'])
+        },3000 )
+        
+    })
+}
 
-console.log('End')
+async function displayUser() {
+    const loggedin = await loginUser('ed',13485)
+    const videos = await userVideos(loggedin.userEmail)
+    console.log(videos)
+}
+
+displayUser()
