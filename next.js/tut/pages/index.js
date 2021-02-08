520a65b74ec693b579a3e613dabeea051423bdda
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-08 15:02:52 +0330

    Learn how to work with layout component and make specific layour for website pages

--- next.js/tut/pages/index.js
+++ next.js/tut/pages/index.js
@@ -8,12 +8,10 @@ import Link from 'next/link'
 export default function Home() {
   return (  
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <Link href="/lists">
         <a> See our lists</a>
       </Link>
-      <Footer />
     </div>
 
   )
