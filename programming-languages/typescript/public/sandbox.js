5fa8100f8ba5c9b89d76d2e9faeab2c1c5059a85
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-13 11:15:12 +0330

    Learn how to work with DOM and Type Casting in TS

--- programming-languages/typescript/public/sandbox.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var greet;
-greet = function (name, greeting) {
-    console.log(name + " says " + greeting);
-};
-greet('Reza', 'Whatsup ma boyyyyyy!');
--- programming-languages/typescript/src/sandbox.ts
+++
@@ -1,7 +0,0 @@
-let greet: (a: string, b: string) => void 
-
-greet = (name: string, greeting: string) => {
-    console.log(`${name} says ${greeting}`)
-}
-
-greet('Reza','Whatsup ma boyyyyyy!')
