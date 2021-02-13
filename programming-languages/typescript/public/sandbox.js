65627f66833f769ed20586e574add45da6a317c1
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-13 11:00:56 +0330

    Learn about type aliases in TS

--- programming-languages/typescript/public/sandbox.js
+++ programming-languages/typescript/public/sandbox.js
@@ -1,16 +1,5 @@
 "use strict";
-var greet;
-greet = function () {
-    console.log('test');
+var printOut = function (char) {
+    console.log(char);
 };
-var add = function (a, b, c) {
-    if (c === void 0) { c = 10; }
-    console.log(a + b);
-    console.log(c);
-};
-add(5, 4, '20');
-var minus = function (a, b) {
-    return a + b;
-};
-var result = minus(10, 7);
-// will automatically change result value to number
+printOut(8);
--- programming-languages/typescript/src/sandbox.ts
+++ programming-languages/typescript/src/sandbox.ts
@@ -1,20 +1,8 @@
-let greet: Function;
+type StringOrNum = string|number 
 
-greet = () => {
-    console.log('test')
+
+const printOut = (char: StringOrNum) => {
+    console.log(char) 
 }
 
-const add = (a:number, b: number, c: number|string = 10) => {
-    console.log(a + b)
-    console.log(c)
-}
-
-add(5,4,'20')
-
-const minus = (a: number,b: number): number => {
-    return a + b
-}
-
-let result = minus(10,7)
-
-// will automatically change result value to number
+printOut(8)
