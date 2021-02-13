585e7726ba029d7146bc5458d76b72d1d42bd402
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-13 11:37:20 +0330

    Learn about modules in TS

--- programming-languages/typescript/public/app.js
+++ programming-languages/typescript/public/app.js
@@ -1,26 +1,15 @@
-"use strict";
 // classes
-var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes " + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
-var invOne = new Invoice('reza', 'poole ab', 30000);
+import { Invoice } from './classes/invoice.js';
+const invOne = new Invoice('reza', 'poole ab', 30000);
 console.log(invOne.format(), invOne.details);
 console.log(invOne);
-var form = document.querySelector('.new-item-form');
+const form = document.querySelector('.new-item-form');
 // console.log(form.children)
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- programming-languages/typescript/src/app.ts
+++ programming-languages/typescript/src/app.ts
@@ -1,19 +1,5 @@
 // classes
-class Invoice {
-    readonly client: string;
-    private details: string;
-    public amount: number;
-
-    constructor(c: string, d: string, a:number){
-        this.client = c
-        this.details = d 
-        this.amount = a
-    }
-
-    format() {
-        return `${this.client} owes ${this.amount} for ${this.details}`
-    }
-}
+import {Invoice} from './classes/invoice.js'
 
 const invOne = new Invoice('reza','poole ab',30000)
 
