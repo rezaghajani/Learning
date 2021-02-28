a1928db4bc5f602e29d66904c2ddf24bdd8c9d2f
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-28 18:58:24 +0330

    Web3 init

---
+++ programming-languages/web3/index.js
@@ -0,0 +1,7 @@
+var Web3 = require('web3')
+
+var url = 'https://1p7ENMTratsqhWuxVfnR63RhMZa:97dd35787a8e7aa649b9f6927f257ce4@eth2-beacon-mainnet.infura.io'
+
+var web3 = new Web3(url)
+
+console.log(web3,'hello world!')
